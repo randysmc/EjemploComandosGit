8ef129ceaa296f667c1378949a960de66944085e
Randy <randy26smc@gmail.com>
2025-08-29 09:46:11 -0600

    agregando división en la develop

--- app/models/Calculadora.php
+++ app/models/Calculadora.php
@@ -13,6 +13,15 @@ class Calculadora{
     public function multiplicar($num1, $num2){
         return $num1 * $num2;
     }
+
+    public function dividir($num1, $num2){
+        if($num2 == 0){
+            throw new InvalidArgumentException("No se puede dividir entre cero");
+            
+        } else {
+            return $num1 / $num2;
+        }
+    }
 }
 
 ?>
